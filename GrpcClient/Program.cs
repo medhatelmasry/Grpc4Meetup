fa092a8bd5407863ad9cb128c7968b3fa8ab8759
Medhat Elmasry <A00127241@ad.bcit.ca>
2019-10-31 09:20:56 -0700

    1st commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,100 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using GrpcServer.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program {
+
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest { Name = "Jane Bond" };
+
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new CustomerRemote.CustomerRemoteClient(channel);
+
+            //var custInput = new CustomerLookupModel { CustomerId = "FISSA" };
+            //var custReply = await customerClient.GetCustomerInfoAsync(custInput);
+            //Console.WriteLine($"{custReply.CompanyName} {custReply.City}");
+
+            CustomerModel newCust = new CustomerModel()
+            {
+                CustomerId = "AAAA",
+                CompanyName = "DDDD",
+                ContactName = "TTTT",
+                City = "Toronto",
+                Country = "Canada"
+            };
+
+            // await insertCustomer(channel, newCust);
+
+            //await updateCustomer(channel, newCust);
+
+            await deleteCustomer(channel, "AAAA");
+
+            await displayAllCustomers(channel);
+
+            Console.ReadLine();
+        }
+
+        static async Task findCustomerById(GrpcChannel channel, string id)
+        {
+            var customerClient = new CustomerRemote.CustomerRemoteClient(channel);
+
+            var custInput = new CustomerLookupModel { CustomerId = id };
+            var custReply = await customerClient.GetCustomerInfoAsync(custInput);
+            Console.WriteLine($"{custReply.CompanyName} {custReply.City}");
+        }
+
+        static async Task insertCustomer(GrpcChannel channel, CustomerModel cust)
+        {
+            var customerClient = new CustomerRemote.CustomerRemoteClient(channel);
+
+            var reply = await customerClient.InsertCustomerAsync(cust);
+            Console.WriteLine(reply.Result);
+        }
+
+        static async Task updateCustomer(GrpcChannel channel, CustomerModel cust)
+        {
+            var customerClient = new CustomerRemote.CustomerRemoteClient(channel);
+
+            var reply = await customerClient.UpdateCustomerAsync(cust);
+            Console.WriteLine(reply.Result);
+        }
+
+        static async Task deleteCustomer(GrpcChannel channel, string id)
+        {
+            var customerClient = new CustomerRemote.CustomerRemoteClient(channel);
+
+            var custInput = new CustomerLookupModel { CustomerId = id };
+            var reply = await customerClient.DeleteCustomerAsync(custInput);
+            Console.WriteLine(reply.Result);
+        }
+
+        static async Task displayAllCustomers(GrpcChannel channel)
+        {
+            var customerClient = new CustomerRemote.CustomerRemoteClient(channel);
+
+            var empty = new Empty();
+            var list = await customerClient.RetrieveAllCustomersAsync(empty);
+
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>++++++++++++<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine($"{item.CustomerId}: {item.CompanyName}");
+            }
+        }
+
+
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,198 @@
+﻿using Grpc.Core;
+using GrpcServer.NW;
+using GrpcServer.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : CustomerRemote.CustomerRemoteBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        private readonly NorthwindContext _context;
+
+        public CustomersService(ILogger<CustomersService> logger, NorthwindContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            Customers c = _context.Customers.Find(request.CustomerId);
+
+            _logger.LogInformation("Sending Customer response");
+
+            if (c != null)
+            {
+                output.CustomerId = c.CustomerId;
+                output.CompanyName = c.CompanyName;
+                output.ContactName = c.ContactName;
+                output.City = c.City;
+                output.Country = c.Country;
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override Task<Reply> InsertCustomer(CustomerModel request, ServerCallContext context)
+        {
+            var c = _context.Customers.Find(request.CustomerId);
+
+            if (c != null)
+            {
+                return Task.FromResult(
+                  new Reply()
+                  {
+                      Result = $"Customer {request.CompanyName} already exists.",
+                      IsOk = false
+                  }
+                );
+            }
+
+            Customers customer = new Customers()
+            {
+                CustomerId = request.CustomerId,
+                CompanyName = request.CompanyName,
+                ContactName = request.ContactName,
+                City = request.City,
+                Country = request.Country
+            };
+
+            _logger.LogInformation("Sending Customer response");
+
+            try
+            {
+                _context.Customers.Add(customer);
+                var returnVal = _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return Task.FromResult(
+               new Reply()
+               {
+                   Result = $"Customer {request.CompanyName} was successfully inserted.",
+                   IsOk = true
+               }
+            );
+        }
+
+        public override Task<Reply> UpdateCustomer(CustomerModel request, ServerCallContext context)
+        {
+            var c = _context.Customers.Find(request.CustomerId);
+
+            if (c == null)
+            {
+                return Task.FromResult(
+                  new Reply()
+                  {
+                      Result = $"Customer {request.CompanyName} cannot be found.",
+                      IsOk = false
+                  }
+                );
+            }
+
+            c.CompanyName = request.CompanyName;
+            c.ContactName = request.ContactName;
+            c.City = request.City;
+            c.Country = request.Country;
+
+            _logger.LogInformation("Sending Customer response");
+
+            try
+            {
+                var returnVal = _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return Task.FromResult(
+               new Reply()
+               {
+                   Result = $"Customer {request.CompanyName} was successfully updated.",
+                   IsOk = true
+               }
+            );
+        }
+
+        public override Task<Reply> DeleteCustomer(CustomerLookupModel request, ServerCallContext context)
+        {
+            var c = _context.Customers.Find(request.CustomerId);
+
+            if (c == null)
+            {
+                return Task.FromResult(
+                  new Reply()
+                  {
+                      Result = $"Customer ID {request.CustomerId} cannot be found.",
+                      IsOk = false
+                  }
+                );
+            }
+
+            _logger.LogInformation("Sending Customer response");
+
+            try
+            {
+                _context.Customers.Remove(c);
+                var returnVal = _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return Task.FromResult(
+               new Reply()
+               {
+                   Result = $"Customer with {request.CustomerId} was successfully deleted.",
+                   IsOk = true
+               }
+            );
+        }
+
+        public override Task<CustomerList> RetrieveAllCustomers(Empty request, ServerCallContext context)
+        {
+            _logger.LogInformation("Sending Customer response");
+
+            CustomerList list = new CustomerList();
+
+            try
+            {
+                List<CustomerModel> customerList = new List<CustomerModel>();
+
+                var customers = _context.Customers.ToList();
+
+                foreach (Customers c in customers)
+                {
+                    customerList.Add(new CustomerModel()
+                    {
+                        CustomerId = c.CustomerId,
+                        CompanyName = c.CompanyName,
+                        ContactName = c.ContactName,
+                        City = c.City,
+                        Country = c.Country
+                    });
+                }
+
+                list.Items.AddRange(customerList);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return Task.FromResult(list);
+        }
+    }
+}
